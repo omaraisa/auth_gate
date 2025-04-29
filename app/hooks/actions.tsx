bd889279c4370a01217b63f92d1e7d1667529f7a
Omar Elhag | Namaa <oelhag@jda.gov.sa>
2025-04-29 16:14:08 +0300

    Deployment Files Restructured

--- app/hooks/actions.tsx
+++ app/hooks/actions.tsx
@@ -45,7 +45,7 @@ export async function login(prevState: {
     expires: new Date(tokenData.expires),
   });
 
-  redirect('http://192.168.1.60/geoportal/');
+  globalThis.location.href = 'http://192.168.1.60/geoportal/';
   }
 
   return {
