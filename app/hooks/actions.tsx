37073ddfa2ed99a60b68f9fa3a7e84dd734b3a6d
Omar Elhag | Namaa <oelhag@jda.gov.sa>
2025-04-29 14:59:42 +0300

    Deployment Files Restructured

--- app/hooks/actions.tsx
+++ app/hooks/actions.tsx
@@ -2,7 +2,8 @@
 
 import { cookies } from 'next/headers';
 import { z } from 'zod';
-import { serverAuthenticateArcGIS } from '../lib/authenticateArcGIS';
+import { redirect } from 'next/navigation';
+import { serverAuthenticateArcGIS } from  '../lib/authenticateArcGIS';
 
 export async function login(prevState: {
   errors: {
@@ -11,15 +12,15 @@ export async function login(prevState: {
   };
 } | undefined, formData: FormData) {
   const schema = z.object({
-    username: z.string().min(3),
-    password: z.string().min(4),
+  username: z.string().min(3),
+  password: z.string().min(4),
   });
 
   const parsed = schema.safeParse(Object.fromEntries(formData));
   if (!parsed.success) {
-    return {
-      errors: parsed.error.flatten().fieldErrors,
-    };
+  return {
+    errors: parsed.error.flatten().fieldErrors,
+  };
   }
 
   const { username, password } = parsed.data;
@@ -27,34 +28,29 @@ export async function login(prevState: {
   const tokenData = await serverAuthenticateArcGIS(username, password);
 
   if (tokenData) {
-    const cookieStore = await cookies();
-    cookieStore.set('arcgis_token', tokenData.token, {
-      httpOnly: false,
-      secure: true,
-      sameSite: 'lax',
-      path: '/',
-      expires: new Date(tokenData.expires),
-    });
+  const cookieStore = await cookies();
+  cookieStore.set('arcgis_token', tokenData.token, {
+    httpOnly: false,
+    secure: true,
+    sameSite: 'lax',
+    path: '/',
+    expires: new Date(tokenData.expires),
+  });
 
-    cookieStore.set('arcgis_token_expiry', tokenData.expires.toString(), {
-      httpOnly: false,
-      secure: true,
-      sameSite: 'lax',
-      path: '/',
-      expires: new Date(tokenData.expires),
-    });
+  cookieStore.set('arcgis_token_expiry', tokenData.expires.toString(), {
+    httpOnly: false,
+    secure: true,
+    sameSite: 'lax',
+    path: '/',
+    expires: new Date(tokenData.expires),
+  });
 
-    return new Response(null, {
-      status: 302,
-      headers: {
-        Location: '/geoportal/',
-      },
-    });
+  redirect('http://192.168.1.60/geoportal/');
   }
 
   return {
-    errors: {
-      username: ['Invalid username or password'],
-    },
+  errors: {
+    username: ['Invalid username or password'],
+  },
   };
 }
